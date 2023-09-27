4c917bf7128ae3b04cecfb5715ac43837db9bdcc
seymen-elmas <seymennadirelmas@gmail.com>
2023-09-27 19:55:06 +0300

    ios study-binding

---
+++ Training-6/Training-6/ColorSelectorView.swift
@@ -0,0 +1,31 @@
+//
+//  ColorSelectorView.swift
+//  Training-6
+//
+//  Created by Seymen Nadir Elmas on 27.09.2023.
+//
+
+import SwiftUI
+
+struct ColorSelectorView: View {
+    @Binding var selectedColor:Color
+    let colors:[Color] = [.red,.purple,.green,.yellow,.blue]
+    var body: some View {
+        HStack{
+            ForEach(colors,id:\.self){color in
+                Image(systemName:selectedColor == color ?"record.circle.fill": "circle.fill")
+                    .foregroundColor(color)
+                    .onTapGesture {
+                        selectedColor = color
+                    }
+                
+            }
+        }
+    }
+}
+
+struct ColorSelectorView_Previews: PreviewProvider {
+    static var previews: some View {
+        ColorSelectorView(selectedColor: .constant(.blue))
+    }
+}
---
+++ Training-6/Training-6/Training_6App.swift
@@ -0,0 +1,17 @@
+//
+//  Training_6App.swift
+//  Training-6
+//
+//  Created by Seymen Nadir Elmas on 27.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct Training_6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
